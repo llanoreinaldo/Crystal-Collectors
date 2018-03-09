3944c0b38abc5cb361b2cdfe2eac4e2fdc27b85d
Reinaldo Llano <llanoreinaldo@gmail.com>
2018-03-09 15:46:22 -0500

    Commiit Changes to Files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,31 @@
 $(document).ready(function () {
-    
+
 });
 
-var targetNumber = 53;
+var targetNumber = " ";
 
-$("#number-to-guess").text(targetNumber);
+$("#random-score").text(targetNumber);
 
 var crystals = $("#crystals");
 
+//Set counter variables for 
 var counter = 0;
+var wins = 0;
+var losses = 0;
+
+//
+var randomCrystalValue = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 // Now for the hard part. Creating multiple crystals each with their own unique number value.
 
 // We begin by expanding our array to include four options.
-var numberOptions = [10, 5, 3, 7];
+var numberOptions = [];
 
 // Next we create a for loop to create crystals for every numberOption.
-for (var i = 0; i < numberOptions.length; i++) {
+for (var i = 0; i < 4; i++) {
 
     // For each iteration, we will create an imageCrystal
     var imageCrystal = $("<img>");
-
-    // First each crystal will be given the class ".crystal-image".
-    // This will allow the CSS to take effect.
-    imageCrystal.addClass("crystal-image");
-
-    // Each imageCrystal will be given a src link to the crystal image
-    imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
 
     // Each imageCrystal will be given a data attribute called data-crystalValue.
     // This data attribute will be set equal to the array value.
@@ -60,4 +59,3 @@ crystals.on("click", ".crystal-image", function () {
     }
 
 });
-
--- assets/javascript/javatest.js
+++ assets/javascript/javatest.js
@@ -1,27 +1,100 @@
-
- //The player will have to guess the answer, just like in Hangman. This time, though, the player will guess with numbers instead of letters. 
-
 //* There will be four crystals displayed as buttons on the page.
- 
+
 //* The player will be shown a random number at the start of the game.
- 
+//* Each crystal should have a random hidden value between 1 - 12.
+
 //* When the player clicks on a crystal, it will add a specific amount of points to the player's total score. 
- 
-    //* Your game will hide this amount until the player clicks a crystal.
-    //* When they do click one, update the player's score counter.
- 
-    //The player wins if their total score matches the random number from the beginning of the game.
- 
-    //* The player loses if their score goes above the random number.
- 
-    //* The game restarts whenever the player wins or loses.
- 
-    //  * When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
- 
-    //* The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
- 
+
+//* Your game will hide this amount until the player clicks a crystal.
+
+//* When they do click one, update the player's score counter.
+
+//The player wins if their total score matches the random number from the beginning of the game.
+
+//* The player loses if their score goes above the random number.
+
+//* The game restarts whenever the player wins or loses.
+
+//  * When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
+
+//* The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
+
 //Game design notes
- 
- //* The random number shown at the start of the game should be between 19 - 120.
- 
- //* Each crystal should have a random hidden value between 1 - 12.
+
+//* The random number shown at the start of the game should be between 19 - 120.
+
+//Creates initial variables
+var random_result;
+var losses = 0;
+var wins = 0;
+var previousAmt = 0;
+
+
+var resetAndStart = function () {
+
+    $(".crystals").empty(); //empty's the crystals from previous game
+
+    var crystalImages = [
+        "assets/images/tiffany1.jpg",
+        "assets/images/tiffany2.jpg",
+        "assets/images/tiffany3.jpg",
+        "assets/images/tiffany4.jpg"];
+
+    //Sets the random result anywhere between 19 and 102
+    random_result = Math.floor((Math.random() * 102) + 19);
+
+    $("#results").html("Random Result: " + random_result);
+
+    //Creates crystals and random values and assigns it 
+    for (var i = 0; i < 4; i++) {
+
+        //Assigns random value between 1-12 to Crystal
+        var random = Math.floor((Math.random() * 11) + 1);
+
+
+        //Creates crystals on HTML and assigns the random value
+        var crystal = $("<div>");
+        crystal.attr({
+            "class": 'crystal anime',
+            "data-random": random,
+        });
+        crystal.css({
+            "background-image": "url('"+ crystalImages[i]+"')",
+            "background-size": "cover"
+        });
+
+        $(".crystals").append(crystal)
+
+    }
+    $("#score").html("Your Score: " + previousAmt);
+}
+
+resetAndStart();
+
+$(document).on("click", ".crystal", function () {
+
+    var num = parseInt($(this).attr("data-random")); //Gets number from crystal and turns it into a number.
+
+
+    previousAmt += num; //adds Crystal number to the previoius amount.
+    $("#score").html("Your Score: " + previousAmt);
+
+
+    if (previousAmt > random_result) {
+        losses++;
+        $("#losses").html("Losses: " + losses);
+        previousAmt = 0;
+        console.log("Sorry, but you lost!! Play again!");
+        resetAndStart();
+
+
+    } else if (previousAmt === random_result) {
+        wins++;
+
+        $("#wins").html("Wins: " + wins);
+        console.log("Congratulations, you won!");
+        previousAmt = 0;
+        resetAndStart();
+    }
+
+});
